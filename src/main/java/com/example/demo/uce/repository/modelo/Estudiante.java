dac224112357506e643ccd11b75f2ab55f57bbbb
Leonardo13344 <lrsuniga@uce.edu.ec>
2022-08-18 15:50:23 -0500

    tareaEst

---
+++ src/main/java/com/example/demo/uce/repository/modelo/Estudiante.java
@@ -0,0 +1,82 @@
+package com.example.demo.uce.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estudiante")
+public class Estudiante {
+
+	@Id
+	@Column(name = "est_id")
+	private Integer id;
+	
+	@Column(name = "est_nombre")
+	private String nombre;
+	
+	@Column(name = "est_apellido")
+	private String apellido;
+	
+	@Column(name = "est_fecha_nacimiento")
+	private LocalDateTime fechaNacimiento;
+	
+	@Column(name = "est_creditos")
+	private Integer creditos;
+	
+	@Column(name = "est_cedula")
+	private String cedula;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Integer getCreditos() {
+		return creditos;
+	}
+
+	public void setCreditos(Integer creditos) {
+		this.creditos = creditos;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/uce/service/IEstudianteService.java
@@ -0,0 +1,12 @@
+package com.example.demo.uce.service;
+
+import com.example.demo.uce.repository.modelo.Estudiante;
+
+public interface IEstudianteService {
+	
+	public void crear(Estudiante estudiante);
+	public void actualizar(Estudiante estudiante);
+	public Estudiante buscarxId(Integer id);
+	public void eliminar(Integer id);
+
+}
