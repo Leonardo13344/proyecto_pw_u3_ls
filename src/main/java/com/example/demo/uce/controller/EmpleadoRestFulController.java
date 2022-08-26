b4e3445cfa650879208ce30e3684bfa8dc414f2a
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2022-08-25 19:49:43 -0500

    taller27

--- src/main/java/com/example/demo/uce/controller/EmpleadoRestFulController.java
+++ src/main/java/com/example/demo/uce/controller/EmpleadoRestFulController.java
@@ -1,11 +1,14 @@
 package com.example.demo.uce.controller;
 
 import java.math.BigDecimal;
+
 import java.util.List;
 
 import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.uce.repository.modelo.Empleado;
@@ -32,9 +36,10 @@ public class EmpleadoRestFulController {
 		try {
 			this.empleadoService.crear(empleado);
 		} catch (Exception e) {
+			
 			mensaje = "Error intente mas tarde";
+			throw new RuntimeException();
 		}
-
 		return mensaje;
 	}
 
@@ -49,6 +54,25 @@ public class EmpleadoRestFulController {
 		Empleado empl = this.empleadoService.buscarxId(idEmpleado);
 		return ResponseEntity.ok(empl);
 	}
+	
+	@GetMapping(path = "/status/{idEmpleado}")
+	public ResponseEntity<Empleado> buscarStatus(@PathVariable("idEmpleado") Integer idEmpleado) {
+		Empleado empl = this.empleadoService.buscarxId(idEmpleado);
+		//return ResponseEntity.status(HttpStatus.OK).body(empl);
+		return ResponseEntity.status(227).body(empl);
+	}
+	
+	@GetMapping(path = "/headers/{idEmpleado}")
+	public ResponseEntity<Empleado> buscarHeaders(@PathVariable("idEmpleado") Integer idEmpleado) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("detalleMensaje", "Estas bien pero envia el apellido adicional");
+		headers.add("solicitud", "Recuerda consumirme mañana");
+		headers.add("valor", "1");
+		Empleado empl = this.empleadoService.buscarxId(idEmpleado);
+		
+		//return ResponseEntity.ok(empl);
+		return new ResponseEntity<>(empl, headers, HttpStatus.OK);
+	}
 
 	@DeleteMapping(path = "/{idEmpleado}")
 	public String eliminar(@PathVariable("idEmpleado") Integer idEmpleado) {
@@ -57,8 +81,16 @@ public class EmpleadoRestFulController {
 	}
 
 	@GetMapping
-	public List<Empleado> buscarEmpleadosSalario(@PathParam(value = "sal") BigDecimal salario) {
+	public List<Empleado> buscarEmpleadosSalario(@RequestParam(value = "salario") BigDecimal salario,
+			@RequestParam(value = "provincia") String provincia) {
+		System.out.println(provincia);
 		return this.empleadoService.buscarSalario(salario);
 	}
+
+	/*
+	 * @GetMapping public List<Empleado> buscarEmpleadosSalario(@PathParam(value =
+	 * "salario") BigDecimal salario) { return
+	 * this.empleadoService.buscarSalario(salario); }
+	 */
 
 }
